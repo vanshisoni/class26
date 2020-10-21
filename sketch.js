4069c07180450e9a679be63b9682310f418006b4
Vanshi <vanshiaaryasoni@gmail.com>
2020-10-21 15:37:08 -0400

    Created a platform

--- sketch.js
+++ sketch.js
@@ -5,6 +5,7 @@ const Bodies = Matter.Bodies;
 var engine, world;
 var box1, pig1;
 var backgroundImg,platform;
+var platform1;
 
 function preload() {
     backgroundImg = loadImage("sprites/bg.png");
@@ -18,6 +19,8 @@ function setup(){
 
     ground = new Ground(600,height,1200,20);
     platform = new Ground(150, 305, 300, 170);
+    platform1 = new Ground(150, 305, 300, 170);
+
 
     box1 = new Box(700,320,70,70);
     box2 = new Box(920,320,70,70);
@@ -61,4 +64,5 @@ function draw(){
 
     bird.display();
     platform.display();
+    platform1.display();
 }
